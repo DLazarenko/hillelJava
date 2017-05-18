bcf22676595c270519b2c1ad6bf1e6ac81024ed0
DLazarenko <daria.lazarenko20@gmail.com>
2017-05-18 23:38:44 +0300

    fixed BankDeposit

--- src/Calculator.java
+++ src/Calculator.java
@@ -15,7 +15,6 @@ import java.util.Scanner;
 public class Calculator {
 
     public static void main(String[] args) {
-
         System.out.println(" Please input first number: ");
         int a = readNumber();
         System.out.println("Your input " + a + " as a first number.");
