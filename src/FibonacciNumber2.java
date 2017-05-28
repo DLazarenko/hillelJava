12eab2e6e5bfb9d23b86d98c809b2c50fb4c3f9e
DLazarenko <daria.lazarenko20@gmail.com>
2017-05-29 00:26:43 +0300

    FibonacciNumber2

--- src/FibonacciNumber2.java
+++ src/FibonacciNumber2.java
@@ -4,19 +4,19 @@
 public class FibonacciNumber2 {
 
     public static void main(String[] args) {
-        System.out.println(fib(5));
+        System.out.println(calculateFib(5));
     }
 
     private static long[] fib = new long[51];
 
-    private static long fib(int number) {
+    private static long calculateFib(int number) {
         if (fib[number] != 0)
             return fib[number];
         else if (number == 0 || number == 1 || number == 2) {
             fib[number] = 1;
             return 1;
         } else {
-            long res = fib(number - 2) + fib(number - 1);
+            long res = calculateFib(number - 2) + calculateFib(number - 1);
             fib[number] = res;
             return res;
         }
