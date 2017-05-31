90ea4083c2bc84527a5cd7f1c427d7040ac05242
DLazarenko <daria.lazarenko20@gmail.com>
2017-06-01 00:25:49 +0300

    Split input value and weight unit

---
+++ src/KgMicrogram.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * Пользователь вводит число, нужно запросить единицы измерения и вывести данные о массе.
+ */
+public class KgMicrogram {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String stroka = scanner.next();
+        System.out.println("Input weight unit.");
+        String weightUnit = scanner.next();
+        int value = Integer.parseInt(stroka);
+        switch (weightUnit) {
+            case "microgram":
+                int microgram = value % 1000;
+                value /= 1000;
+                System.out.print(microgram + " microgram ");
+            case "gram":
+                int gram = value % 1000;
+                value /= 1000;
+                System.out.print(gram + " gram ");
+            case "kg":
+                int kg = value % 1000;
+                value /= 1000;
+                System.out.print(kg + " kg ");
+            case "t":
+                int t = value;
+                System.out.print(t + " t ");
+        }
+    }
+}
