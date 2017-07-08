d6d642feb44e9a2cb1e5f19f6ef50f59693158c1
DLazarenko <daria.lazarenko20@gmail.com>
2017-07-09 00:48:44 +0300

    Map with put() and get()

--- src/lesson15/Map.java
+++ src/lesson15/Map.java
@@ -4,15 +4,22 @@ public class Map {
     Entry[] list = new Entry[0];
 
     public void put(Object key, Object value) {
-        Entry[] newList = new Entry[list.length + 1];
         for (int i = 0; i < list.length; i++) {
-            newList[i] = list[i];
+            if (list[i].getKey().equals(key)) {
+                list[i].setValue(value);
+            } else {
+                Entry[] newList = new Entry[list.length + 1];
+                for (int k = 0; k < list.length; k++) {
+                    newList[k] = list[k];
+                }
+                Entry entry = new Entry();
+                entry.setKey(key);
+                entry.setValue(value);
+                newList[list.length] = entry;
+                list = newList;
+            }
         }
-        Entry entry = new Entry();
-        entry.setKey(key);
-        entry.setValue(value);
-        newList[list.length] = entry;
-        list = newList;
+
     }
 
     public Object get(Object key) {
