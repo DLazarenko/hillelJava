49a1b5741cb0fa015172c5f60a8208f2cd88fcae
DLazarenko <daria.lazarenko20@gmail.com>
2017-07-09 00:55:49 +0300

    Map with put() and get()

--- src/lesson15/Main.java
+++ src/lesson15/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Map map = new Map();
         map.put("ivanov", "ivan");
+        System.out.println(map.get("ivanov"));
+
         map.put("yyyy", "12");
+        map.put("ivanov", "vlad");
         System.out.println(map);
         System.out.println(map.get("ivanov"));
     }
--- src/lesson15/Map.java
+++ src/lesson15/Map.java
@@ -4,22 +4,23 @@ public class Map {
     Entry[] list = new Entry[0];
 
     public void put(Object key, Object value) {
-        for (int i = 0; i < list.length; i++) {
-            if (list[i].getKey().equals(key)) {
-                list[i].setValue(value);
-            } else {
-                Entry[] newList = new Entry[list.length + 1];
-                for (int k = 0; k < list.length; k++) {
-                    newList[k] = list[k];
+        if (list.length != 0) {
+            for (int i = 0; i < list.length; i++) {
+                if (list[i].getKey().equals(key)) {
+                    list[i].setValue(value);
                 }
-                Entry entry = new Entry();
-                entry.setKey(key);
-                entry.setValue(value);
-                newList[list.length] = entry;
-                list = newList;
             }
+        } else {
+            Entry[] newList = new Entry[list.length + 1];
+            for (int k = 0; k < list.length; k++) {
+                newList[k] = list[k];
+            }
+            Entry entry = new Entry();
+            entry.setKey(key);
+            entry.setValue(value);
+            newList[list.length] = entry;
+            list = newList;
         }
-
     }
 
     public Object get(Object key) {
