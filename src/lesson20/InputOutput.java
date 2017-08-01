ce3812d0bb5a3ce62e9157afa1e74caa9f7c9c80
DLazarenko <daria.lazarenko20@gmail.com>
2017-08-01 15:28:36 +0300

    InputStream OutputStream

--- src/lesson20/InputOutput.java
+++ src/lesson20/InputOutput.java
@@ -7,16 +7,18 @@ public class InputOutput {
     public static void main(String[] args) throws IOException {
         String firstFileName = "/Users/admin/IdeaProjects/hillelJava/src/lesson20/FirstFile";
         String secondFileName = "/Users/admin/IdeaProjects/hillelJava/src/lesson20/SecondFile";
-        InputStream inputStream = new FileInputStream(firstFileName);
+        InputStream inputStream = new BufferedInputStream(new FileInputStream(firstFileName));
         OutputStream outputStream = new FileOutputStream(secondFileName);
         int data = inputStream.read();
         outputStream.write(data);
-
+        long start = System.currentTimeMillis();
         while (data != -1) {
+
             data = inputStream.read();
             outputStream.write(data);
             System.out.println(data);
         }
+        System.out.println(System.currentTimeMillis() - start + " milliseconds time");
         inputStream.close();
         outputStream.close();
     }
